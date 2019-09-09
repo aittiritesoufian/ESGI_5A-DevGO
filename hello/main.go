9c71ffc5ed13f64a70ed1c2eec432e912d224bed
Soufian AIT TIRITE <aittirite.soufian@gmail.com>
2019-09-09 18:05:34 +0200

    update

---
+++ hello/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	Display("Salut a tous")
+}
+
+// Print the given message in the standard output
+func Display(msg string){
+	fmt.Println("Salut a tous")
+}
