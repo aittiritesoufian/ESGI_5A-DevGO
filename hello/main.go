223f69eb604dcdf8ea209bb6fb34b4843a2abd14
Soufian AIT TIRITE <aittirite.soufian@gmail.com>
2019-09-09 19:54:45 +0200

    update

--- hello/main.go
+++ hello/main.go
@@ -1,12 +1,29 @@
 package main
 
 import "fmt"
+import "time"
 
 func main(){
-	Display("Salut a tous")
+	if true {
+		Display("coucou")
+	}
+	fmt.Println(Display("Salut a tous"))
 }
 
 // Print the given message in the standard output
-func Display(msg string){
-	fmt.Println("Salut a tous")
+func Display(msg string) string {
+	switch msg {
+		case "coucou":
+			fmt.Println("Coucou !")
+		case "test":
+			fmt.Println("Test !!!")
+		case "the end":
+			fmt.Println("The End !!!!")
+		default:
+			fmt.Println("It's default!")
+	}
+	var t = time.Now()
+	fmt.Println("The current time is : ")
+	fmt.Println(t)
+	return "plop"
 }
