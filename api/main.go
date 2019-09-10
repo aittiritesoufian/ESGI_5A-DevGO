068562f463b6b164febd2abd65ff7fe9fba64d90
Soufian AIT TIRITE <aittirite.soufian@gmail.com>
2019-09-10 20:16:58 +0200

    update

--- api/main.go
+++ api/main.go
@@ -1,28 +1,37 @@
 package main
 
-import "fmt"
-import "time"
-import "encoding/json"
+import (
+	"fmt"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	now := time.Now()
-    u := User {
-		FirstName:"Henri",
-		LastName:"Lepic",
-		DateOfBirth:now,
+	u := User{
+		FirstName:   "Henri",
+		LastName:    "Lepic",
+		DateOfBirth: now,
 	}
 	fmt.Println(u)
-	payload,err := json.Marshal(u)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(payload))
+	//payload, err := json.Marshal(u)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println(string(payload))
+
+	r := gin.Default()
+	r.GET("/user", func(c *gin.Context) {
+		c.JSON(200, u)
+	})
+	r.Run(":8080")
 }
 
-type User struct{
-	FirstName string `json:"first_name"`
-	LastName string
-	DateOfBirth time.Time
+type User struct {
+	FirstName   string    `json:"first_name"`
+	LastName    string    `json:"last_name"`
+	DateOfBirth time.Time `json:"birth_date"`
 }
 
 func (u User) String() string {
